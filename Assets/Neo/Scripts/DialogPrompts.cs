3398ecc8d2707017417f9bca155f32b7b7d24ae6
Neo <sengyothinh.neo72@gmail.com>
2020-02-19 16:00:40 +1100

    Simple Dialogue Stuff
    Dialogue plays on button press

---
+++ Assets/Neo/Scripts/DialogPrompts.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogPrompts : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public Text dialogueText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialogue dialogue)
+    {
+        print("Dialogue starts");
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //print(sentence);
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        print("Fin");
+        dialogueText.text = "";
+    }
+}
---
+++ Assets/Neo/Scripts/DialogueTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public float timeLeft = 10;
+
+    public bool countdownEnd;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(countdownEnd = true)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                Debug.Log(timeLeft);
+            }
+            if(timeLeft == 0)
+            {
+                countdownEnd = false;
+            }
+        }
+
+        if (countdownEnd = false)
+        {
+            TriggerDialogue();
+            
+            countdownEnd = true;
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogPrompts>().StartDialog(dialogue);
+    }
+}
