9309bf3f0ab5b017082194c775832b3cfd9f9332
Neo <sengyothinh.neo72@gmail.com>
2020-02-26 16:10:54 +1100

    XCvbfgcjm vbmn jfcgnhdf

--- Assets/Neo/Scripts/Dialogue.cs
+++ Assets/Neo/Scripts/Dialogue.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
 public class Dialogue : ScriptableObject
 {
     [TextArea(3, 10)]
     public string[] sentences;
+
+    public DialogueChoices choice;
+
+    public Dialogue newDialogue;
 }
+
+
