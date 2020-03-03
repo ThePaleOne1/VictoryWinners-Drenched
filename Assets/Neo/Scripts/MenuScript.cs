e3c79efed9fd9e8ba37fa3aac4e8a7c3851982d2
Neo <sengyothinh.neo72@gmail.com>
2020-03-03 11:54:54 +1100

    BACKTOMENUNEXT
    there's a menu

---
+++ Assets/Neo/Scripts/MenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public GameObject playMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+
+        playMenu.SetActive(false);
+    }
+
+    public void ClickPlay()
+    {
+        mainMenu.SetActive(false);
+
+        playMenu.SetActive(true);
+
+        print("play");
+    }
+
+    public void ClickBack()
+    {
+        mainMenu.SetActive(true);
+
+        playMenu.SetActive(false);
+
+        print("BACKTOMENU");
+    }
+
+    public void ClickQuit()
+    {
+        Application.Quit();
+
+        print("see you next time...");
+    }
+
+    
+}
