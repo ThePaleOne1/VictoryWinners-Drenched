714cad90dbb70400a31d54085fbb6b48665175fb
Neo <sengyothinh.neo72@gmail.com>
2020-03-06 19:09:18 +1100

    few HUD updates
    added a variable to drain health and food bars. the hud numbers can be shown by holding tab down.

--- Assets/Neo/Scripts/StatusScript.cs
+++ Assets/Neo/Scripts/StatusScript.cs
@@ -17,22 +17,56 @@ public class StatusScript : MonoBehaviour
 
     public float currentResource;
 
+    public float drainHealth;
+
+    public float drainFood;
+
     public Slider healthSlider;
 
     public Slider foodSlider;
 
     public Slider resourceSlider;
+
+    public Text healthNumber;
+
+    public Text foodNumber;
+
+    public Text resourceNumber;
     
     // Start is called before the first frame update
     void Start()
     {
         setStatus();
+
+        hideStatus();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentHealth > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentHealth -= 10;
+
+                updateStatus();
+
+                Debug.Log("ouchies");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            showStatus();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            hideStatus();
+        }
+
+        drainStatus();
     }
 
     public void setStatus()
@@ -54,5 +88,60 @@ public class StatusScript : MonoBehaviour
         resourceSlider.maxValue = maxResource;
 
         resourceSlider.value = maxResource;
+
+        healthNumber.text = "" + currentHealth.ToString("f0");
+
+        foodNumber.text = "" + currentFood.ToString("f0");
+
+        resourceNumber.text = "" + currentResource.ToString("f0");
+    }
+
+    public void drainStatus()
+    {
+        if (currentFood <= 0)
+        {
+            if(currentHealth > 5)
+            {
+                currentHealth -= drainHealth * Time.deltaTime;
+
+                updateStatus();
+            }      
+        }
+
+        if (currentFood > 0)
+        {
+            currentFood -= drainFood * Time.deltaTime;
+
+            updateStatus();
+        }
+    }
+
+    public void showStatus()
+    {
+        healthNumber.gameObject.SetActive(true);
+
+        foodNumber.gameObject.SetActive(true);
+
+        resourceNumber.gameObject.SetActive(true);
+    }
+
+    public void hideStatus()
+    {
+        healthNumber.gameObject.SetActive(false);
+
+        foodNumber.gameObject.SetActive(false);
+
+        resourceNumber.gameObject.SetActive(false);
+    }
+
+    public void updateStatus()
+    {
+        healthSlider.value = currentHealth;
+
+        healthNumber.text = "" + currentHealth.ToString("f0");
+
+        foodSlider.value = currentFood;
+
+        foodNumber.text = "" + currentFood.ToString("f0");
     }
 }
