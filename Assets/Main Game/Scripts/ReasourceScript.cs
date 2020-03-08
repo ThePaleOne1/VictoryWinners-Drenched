fef522a14f89ac2a966de750d94620c4b21c44b6
Lachlan <mindcraftlachlan@outlook.com>
2020-03-08 21:12:42 +1100

    i did a things

---
+++ Assets/Main Game/Scripts/ReasourceScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReasourceScript : MonoBehaviour
+{
+    [SerializeField]
+    float cooldown = 0.5f;
+    bool canInteract = true;
+
+    [SerializeField]
+    ResourceMeter resourceMeter;
+    
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (Input.GetMouseButtonDown(0) && canInteract) 
+        {
+            canInteract = false;
+            anim.SetTrigger("Hit");
+            Invoke("CanInteract", cooldown);
+
+
+            if (col.tag == "Tree")
+            {
+                resourceMeter.wood += 1;
+            }
+        }
+    }
+
+    void CanInteract()
+    {
+        canInteract = true;
+    }
+}
---
+++ Assets/Main Game/Scripts/ResourceMeter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceMeter : MonoBehaviour
+{
+    public float wood;
+    public float food;
+    public float sanity;
+}
