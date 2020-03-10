300d0ff9f11546609db1b5f25603c3f23bc41ee9
Neo <sengyothinh.neo72@gmail.com>
2020-03-10 21:06:50 +1100

    HUD Updated

--- Assets/Main Game/Scripts/StatusScript.cs
+++ Assets/Main Game/Scripts/StatusScript.cs
@@ -10,7 +10,9 @@ public class StatusScript : MonoBehaviour
     public float maxResource = 100;
     public float maxSanity = 100;
 
-    private float currentResource;
+    private float currentWood;
+    private float currentFiber;
+    private float currentFlint;
     //public float currentHealth;
     private float currentFood;
     private float currentSanity;
@@ -19,12 +21,20 @@ public class StatusScript : MonoBehaviour
 
     public Slider healthSlider;
     public Slider foodSlider;
-    public Slider resourceSlider;
+    public Slider woodSlider;
+    public Slider fiberSlider;
+    public Slider flintSlider;
     public Slider sanitySlider;
 
-    public Text healthNumber;
+    public Text healthText;
+    public Text foodText;
+    public Text sanityText;
+
+    public Text healthNumber;   
     public Text foodNumber;
-    public Text resourceNumber;
+    public Text woodNumber;
+    public Text fiberNumber;
+    public Text flintNumber;
     public Text sanityNumber;
 
     [SerializeField] private ResourceMeter StatusStuff;
@@ -71,7 +81,9 @@ public class StatusScript : MonoBehaviour
     {
         StatusStuff.Health = maxHealth;
         currentFood = StatusStuff.food;
-        currentResource = StatusStuff.wood;
+        currentWood = StatusStuff.wood;
+        currentFiber = StatusStuff.fiber;
+        currentFlint = StatusStuff.flint;
         currentSanity = StatusStuff.sanity;
 
         healthSlider.maxValue = maxHealth;
@@ -80,15 +92,23 @@ public class StatusScript : MonoBehaviour
         foodSlider.maxValue = maxFood;
         foodSlider.value = maxFood;
 
-        resourceSlider.maxValue = maxResource;
-        resourceSlider.value = currentResource;
+        woodSlider.maxValue = maxResource;
+        woodSlider.value = currentWood;
+
+        fiberSlider.maxValue = maxResource;
+        fiberSlider.value = currentFiber;
+
+        flintSlider.maxValue = maxResource;
+        flintSlider.value = currentFlint;
 
         sanitySlider.maxValue = maxSanity;
         sanitySlider.value = currentSanity;
 
         healthNumber.text = "" + StatusStuff.Health.ToString("f0");
         foodNumber.text = "" + currentFood.ToString("f0");
-        resourceNumber.text = "" + currentResource.ToString("f0");
+        woodNumber.text = "" + currentWood.ToString("f0");
+        fiberNumber.text = "" + currentFiber.ToString("f0");
+        flintNumber.text = "" + currentFlint.ToString("f0");
         sanityNumber.text = "" + currentSanity.ToString("f0");
     }
 
@@ -108,24 +128,38 @@ public class StatusScript : MonoBehaviour
     public void showStatus()
     {
         healthNumber.gameObject.SetActive(true);
+        healthText.gameObject.SetActive(false);
 
         foodNumber.gameObject.SetActive(true);
+        foodText.gameObject.SetActive(false);
 
-        resourceNumber.gameObject.SetActive(true);
+        woodNumber.gameObject.SetActive(true);
+
+        fiberNumber.gameObject.SetActive(true);
+
+        flintNumber.gameObject.SetActive(true);
 
         sanityNumber.gameObject.SetActive(true);
+        sanityText.gameObject.SetActive(false);
     }
 
     //hides numbers on tab release
     public void hideStatus()
     {
         healthNumber.gameObject.SetActive(false);
+        healthText.gameObject.SetActive(true);
 
         foodNumber.gameObject.SetActive(false);
+        foodText.gameObject.SetActive(true);
 
-        resourceNumber.gameObject.SetActive(false);
+        woodNumber.gameObject.SetActive(false);
+
+        fiberNumber.gameObject.SetActive(false);
+
+        flintNumber.gameObject.SetActive(false);
 
         sanityNumber.gameObject.SetActive(false);
+        sanityText.gameObject.SetActive(true);
     }
 
     //updates any changes to statuses
@@ -138,9 +172,17 @@ public class StatusScript : MonoBehaviour
         foodSlider.value = currentFood;
         foodNumber.text = "" + currentFood.ToString("f0");
 
-        currentResource = StatusStuff.wood;
-        resourceSlider.value = currentResource;
-        resourceNumber.text = "" + currentResource.ToString("f0");
+        currentWood = StatusStuff.wood;
+        woodSlider.value = currentWood;
+        woodNumber.text = "" + currentWood.ToString("f0");
+
+        currentFiber = StatusStuff.fiber;
+        fiberSlider.value = currentFiber;
+        fiberNumber.text = "" + currentFiber.ToString("f0");
+
+        currentFlint = StatusStuff.flint;
+        flintSlider.value = currentFlint;
+        flintNumber.text = "" + currentFlint.ToString("f0");
 
         currentSanity = StatusStuff.sanity;
         sanitySlider.value = currentSanity;
