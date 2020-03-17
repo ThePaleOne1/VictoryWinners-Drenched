1b4bcd9440f76f19f6c84f3148a9deef726d1b6a
Neo <sengyothinh.neo72@gmail.com>
2020-03-17 13:22:44 +1100

    asdffgbfgh

--- Assets/Neo/Scripts/ClockScript.cs
+++ Assets/Neo/Scripts/ClockScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ClockScript : MonoBehaviour
 {
+    public Transform from;
+
+    public Transform to;
+
+    public float rotateSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.RotateTowards(from.rotation, to.rotation, Time.deltaTime * rotateSpeed);
     }
 }
