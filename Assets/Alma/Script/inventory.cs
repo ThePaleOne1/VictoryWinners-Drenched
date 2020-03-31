59db3e48d763eb4751352317df89228738dd1781
Alma <willalma123@gmail.com>
2020-03-31 16:06:33 +1100

    I added skybox, for nicer view. Fixed up recipe book and buttons.

---
+++ Assets/Alma/Script/inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public GameObject oneSlot;
+    public GameObject notSelected;
+    public GameObject CraftComplete;
+    public bool ifSelected;
+
+    public void Start()
+    {
+        oneSlot.gameObject.SetActive(false);
+        notSelected.gameObject.SetActive(true);
+        CraftComplete.gameObject.SetActive(false);
+        ifSelected = false;
+    }
+
+    public void firstSlot()
+    {
+        ifSelected = true;
+        oneSlot.gameObject.SetActive(true);
+        notSelected.gameObject.SetActive(false);
+    }
+
+    public void Craft()
+    {
+        if (ifSelected)
+        {
+            CraftComplete.gameObject.SetActive(true);
+        }
+        
+    }
+
+}
