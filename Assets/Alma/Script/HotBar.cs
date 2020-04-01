a944c7e942cc7982a83b8d0b40646bffd683d443
Alma <willalma123@gmail.com>
2020-04-01 19:37:38 +1100

    Added Jake's little coconut didili didili song

--- Assets/Alma/Script/HotBar.cs
+++ Assets/Alma/Script/HotBar.cs
@@ -23,6 +23,24 @@ public class HotBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(HotbarSlot.childCount != 0 && thisSlot.childCount == 1)
+        {
+            for(int i = 0; i < Items.Length; i++)
+            {
+                if (HotbarSlot.GetChild(0).name == Items[i].name)
+                {
+                    InstObject = (GameObject)(Instantiate(Items[i].gameObject, Vector3.zero, Quaternion.identity));
+                    InstObject.transform.SetParent(thisSlot.transform);
+                    InstObject.transform.localPosition = Vector3.zero;
+                    InstObject.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+                    thisSlot.GetChild(1).GetComponent<BoxCollider>().enabled = false;
+                    return;
+                }
+            }
+            
+        }
+
+
         if(Input.mouseScrollDelta.y >= 1)
         {
             scrollPosition++;
