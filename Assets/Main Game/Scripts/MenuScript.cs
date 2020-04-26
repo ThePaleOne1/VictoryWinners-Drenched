033a1dce0c467801ede61081feb05cf705657f6a
Neo <sengyothinh.neo72@gmail.com>
2020-04-26 21:44:43 +1000

    updated menu with how to play section

--- Assets/Main Game/Scripts/MenuScript.cs
+++ Assets/Main Game/Scripts/MenuScript.cs
@@ -8,14 +8,16 @@ public class MenuScript : MonoBehaviour
 {
     public GameObject mainMenu;
 
-    public GameObject playMenu;
-    
+    public GameObject Controls;
+
+    public GameObject Status;
+
+    public GameObject Crafting;
+
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(true);
-
-        playMenu.SetActive(false);
     }
 
     public void newGame(int index)
@@ -23,20 +25,9 @@ public class MenuScript : MonoBehaviour
         SceneManager.LoadScene(index);
     }
 
-    public void ClickPlay()
-    {
-        mainMenu.SetActive(false);
-
-        playMenu.SetActive(true);
-
-        print("play");
-    }
-
     public void ClickBack()
     {
         mainMenu.SetActive(true);
-
-        playMenu.SetActive(false);
 
         print("BACKTOMENU");
     }
@@ -48,5 +39,24 @@ public class MenuScript : MonoBehaviour
         print("see you next time...");
     }
 
-    
+    public void ClickControls()
+    {
+        Controls.SetActive(true);
+        Status.SetActive(false);
+        Crafting.SetActive(false);
+    }
+
+    public void ClickStatus()
+    {
+        Controls.SetActive(false);
+        Status.SetActive(true);
+        Crafting.SetActive(false);
+    }
+
+    public void ClickCrafting()
+    {
+        Controls.SetActive(false);
+        Status.SetActive(false);
+        Crafting.SetActive(true);
+    }
 }
